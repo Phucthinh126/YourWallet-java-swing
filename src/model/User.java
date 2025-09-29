761e126e7d1baec8c2d07fbbe0289eccd7bdae89
Huynh Phuc Thinh <phucthinhlap100323@gmail.com>
2025-09-29 23:42:28 +0700

    Update new Password

--- src/model/User.java
+++ src/model/User.java
@@ -4,6 +4,7 @@ public class User {
     private final String UER_NAME = "admin";
     private final String USER_PASS = "admin126";
 
+    // get thong tin dang nhap
     public String getUserName() {
         return UER_NAME;
     }
